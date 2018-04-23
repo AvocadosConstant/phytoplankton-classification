7c8e7c0b3ed7f139b34255717e43880a974ecb4e
Shehtab Zaman <szaman5@binghamton.edu>
2018-04-23 10:02:39 -0400

    Fixed cnn.py. Now can check the value of individual images

--- models/cnn.py
+++ models/cnn.py
@@ -29,14 +29,22 @@ images = np.multiply(images, 1.0/255.0)
 x_batch = images.reshape(1, image_size,image_size,num_channels)
 
 
+#s restore the saved model 
+sess = tf.Session()
+# Step-1: Recreate the network graph. At this step only graph is created.
+saver = tf.train.import_meta_graph('phytoplankton-model.meta')
+# Step-2: Now let's load the weights saved using the restore method.
+saver.restore(sess, tf.train.latest_checkpoint('./'))
 graph = tf.get_default_graph()
 
 y_pred = graph.get_tensor_by_name("y_pred:0")
 
-## Let's feed the images to the input placeholders
+## let's feed the images to the input placeholders
 x= graph.get_tensor_by_name("x:0")
 y_true = graph.get_tensor_by_name("y_true:0")
-y_test_images = np.zeros((1, 2))
+y_test_images = np.zeros((1, 9))
 
 feed_dict_testing = {x: x_batch, y_true: y_test_images}
 result=sess.run(y_pred, feed_dict=feed_dict_testing)
+
+print(result)
